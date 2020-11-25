04ee6e55901b290869431ff16d85c60ea845ba85
Sriram Sundararajan <ssunda59@asu.edu>
2020-11-25 13:36:19 -0700

    Integrating innovative view and network chart

--- d3/scripts/innovativechart.js
+++ d3/scripts/innovativechart.js
@@ -1,25 +1,3 @@
-import { network, drawNetworkChart } from './networkchart.js'
-
-export function buttonclick() {
-    console.log("Change")
-}
-
-export function timechange() {
-    let startTime = d3.select('#starttime').property('value')
-    let endTime = d3.select('#endtime').property('value')
-    let date = d3.select('#date').property('value')
-    console.log(Date.parse(date + ' ' + startTime));
-    console.log(Date.parse(date + ' ' + endTime));
-    drawNetworkChart(Date.parse(date + ' ' + startTime), Date.parse(date + ' ' + endTime))
-}
-
-function init() {
-    network();
-}
-
-init()
-
-
 var NameProvider = ["Port Scanning", "FTP/SSH Attack", "SQL Attack", "Data Outage", "DNS Attack"];
 
 var matrix = [
